d9cff4e4a955589996826b44680cadf53404af3d
RandyRand0m <146391529+RandyRand0m@users.noreply.github.com>
2023-12-18 00:04:45 +0500

    src

---
+++ src/core.js
@@ -0,0 +1,152 @@
+//Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
+function isInteger(n) {
+	return (n & 1) === 0;
+	}
+
+//Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
+function even() {
+	let result = [];
+	for (let i = 2; i <= 20; i += 2) {
+		result.push(i);
+	}
+	return result;
+	}	
+
+//Напишите функцию, считающую сумму чисел до заданного используя цикл
+function sumTo(n) {
+	let sum = 0;
+	for (let i = 1; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+	}
+
+//Напишите функцию, считающую сумму чисел до заданного используя рекурсию
+function recSumTo(n) {
+	if (n === 1) {
+		return 1;
+	}
+	return n + recSumTo(n - 1);
+	}
+
+//Напишите функцию, считающую факториал заданного числа
+function factorial(n) {
+	if (n === 0) {
+		return 1;
+	}
+	return n * factorial(n - 1);
+	}
+
+//Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
+function isBinary(n) {
+	if (n === 1) {
+		return true;
+	}
+// если n нечетное или меньше 1, это не степень двойки, возвращаем false
+	if (n % 2 !== 0 || n < 1) {
+		return false;
+	}
+// рекурсивный вызов функции, деля n на 2
+	return isBinary(n / 2);
+}
+
+//Напишите функцию, которая находит N-е число Фибоначчи
+function fibonacci(n) {
+	if (n <= 1) {
+		return n;
+	}
+	return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+/** Напишите функцию, которая принимает начальное значение и функцию операции
+ * и возвращает функцию - выполняющую эту операцию.
+ * Если функция операции (operatorFn) не задана - по умолчанию всегда
+ * возвращается начальное значение (initialValue)
+ * @param initialValue
+ * @param operatorFn - (storedValue, newValue) => {operation}
+ * @example
+ * const sumFn =  getOperationFn(10, (a,b) => a + b);
+ * console.log(sumFn(5)) - 15
+ * console.log(sumFn(3)) - 18
+ */
+function getOperationFn(initialValue, operatorFn) {
+	let storedValue = initialValue;
+
+	if (operatorFn === undefined) {
+		return () => storedValue;
+	}
+
+	return (newValue) => {
+		storedValue = operatorFn(storedValue, newValue);
+		return storedValue;
+	};
+}
+
+/**
+ * Напишите функцию создания генератора арифметической последовательности.
+ * При ее вызове, она возвращает новую функцию генератор - generator().
+ * Каждый вызов функции генератора возвращает следующий элемент последовательности.
+ * Если начальное значение не передано, то оно равно 0.
+ * Если шаг не указан, то по дефолту он равен 1.
+ * Генераторов можно создать сколько угодно - они все независимые.
+ *
+ * @param {number} start - число с которого начинается последовательность
+ * @param {number} step  - число шаг последовательности
+ * @example
+ * const generator = sequence(5, 2);
+ * console.log(generator()); // 5
+ * console.log(generator()); // 7
+ * console.log(generator()); // 9
+ */
+function sequence(start, step) {
+
+	let value = start; // создаем переменную value и присваиваем ей значение start
+
+	function generator() { // объявляем функцию generator
+		const currentValue = value; // создаем константу currentValue и присваиваем ей значение переменной value
+		value += step; // увеличиваем значение value на значение переменной step
+		return currentValue; // возвращаем значение переменной currentValue
+	}
+
+	return generator; // возвращаем функцию generator
+}
+
+/**
+ * Напишите функцию deepEqual, которая принимает два значения
+ * и возвращает true только в том случае, если они имеют одинаковое значение
+ * или являются объектами с одинаковыми свойствами,
+ * значения которых также равны при сравнении с рекурсивным вызовом deepEqual.
+ * Учитывать специфичные объекты(такие как Date, RegExp итп) не обязательно
+ *
+ * @param {object} firstObject - первый объект
+ * @param {object} secondObject - второй объект
+ * @returns {boolean} - true если объекты равны(по содержанию) иначе false
+ * @example
+ * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 33], text: 'text'}) // true
+ * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 3], text: 'text2'}) // false
+ */
+function deepEqual(firstObject, secondObject) {
+	let value = start; // создаем переменную value и присваиваем ей значение start
+
+	function generator() { // объявляем функцию generator
+		const currentValue = value; // создаем константу currentValue и присваиваем ей значение переменной value
+		value += step; // увеличиваем значение value на значение переменной step
+		return currentValue; // возвращаем значение переменной currentValue
+	}
+
+	return generator; 
+}
+
+
+module.exports = {
+    isInteger,
+    even,
+    sumTo,
+    recSumTo,
+    factorial,
+    isBinary,
+    fibonacci,
+    getOperationFn,
+    sequence,
+    deepEqual,
+};
